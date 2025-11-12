22081453ca4971cb57825d6c8fdb295b7ece8abe
Silva1325 <miguelsilva2001511@gmail.com>
2025-11-12 18:13:28 +0000

    Naive bayes

--- 3 - Classification/K-Nearest Neighbors/exercise.py
+++ 3 - Classification/K-Nearest Neighbors/exercise.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import svm
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix,accuracy_score
--- 3 - Classification/Naive bayes/exercise.py
+++ 3 - Classification/Naive bayes/exercise.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix,accuracy_score
 from matplotlib.colors import ListedColormap
 
 # Import Data
-dataset = pd.read_csv(r"Classification\Datasources\social_newtorks_ads_data.csv")
+dataset = pd.read_csv(r"3 - Classification\Datasources\social_newtorks_ads_data.csv")
 
 # Indepentend variables and dependent variable
 X = dataset.iloc[:,:-1].values
@@ -21,11 +21,9 @@ X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.2, random_sta
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-#print(X_train)
-#print(X_test)
 
-# Trainning the Logistic regression model into the whole dataset 
-classifier = LogisticRegression(random_state=0)
+# Trainning the Naive Bayes model into the whole dataset 
+classifier = GaussianNB()
 classifier.fit(X_train,y_train)
 
 # Predict a new result
@@ -53,7 +51,7 @@ plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(['red', 'green'])(i), label = j)
-plt.title('Logistic Regression (Training set)')
+plt.title('Naive Bayes (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -82,7 +80,7 @@ for i, j in enumerate(np.unique(y_set)):
         color=colors[i], label=j
     )
 # Add titles and labels
-plt.title('Logistic Regression (Test set)')
+plt.title('Naive Bayes (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
